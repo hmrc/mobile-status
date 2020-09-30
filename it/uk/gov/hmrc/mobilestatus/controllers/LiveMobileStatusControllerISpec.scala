186dca9c3f99b8899e9426c68e1af0010d610e19
Martin Rolfe <52818407+mrolfe88@users.noreply.github.com>
2020-09-30 12:23:19 +0100

    [HMA-3504] Removed service name from url path added tests to cover invalid/missing journeyId

--- it/uk/gov/hmrc/mobilestatus/controllers/LiveMobileStatusControllerISpec.scala
+++ it/uk/gov/hmrc/mobilestatus/controllers/LiveMobileStatusControllerISpec.scala
@@ -16,11 +16,25 @@ class LiveMobileStatusControllerISpec extends BaseISpec {
   "GET /status" should {
     "return valid response based on config" in {
 
-      val response = await(wsUrl("/mobile-status/status?journeyId=7f1b5289-5f4d-4150-93a3-ff02dda28375").get)
+      val response = await(wsUrl("/status?journeyId=7f1b5289-5f4d-4150-93a3-ff02dda28375").get)
       println(Json.prettyPrint(response.json))
       response.status shouldBe 200
       (response.json \ "feature").as[List[FeatureFlag]].size shouldBe 1
       Json.prettyPrint(response.json) shouldBe(expectedJsonResponse)
+    }
+
+    "return 400 if no journeyId supplied" in {
+
+      val response = await(wsUrl("/status").get)
+      println(Json.prettyPrint(response.json))
+      response.status shouldBe 400
+    }
+
+    "return 400 if invalid journeyId supplied" in {
+
+      val response = await(wsUrl("/status?journeyId=invalidJourneyId").get)
+      println(Json.prettyPrint(response.json))
+      response.status shouldBe 400
     }
   }
 }
